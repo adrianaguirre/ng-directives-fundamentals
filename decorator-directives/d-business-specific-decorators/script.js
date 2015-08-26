c5fa438efadaccb09d884321abf2bca16c22267f
Adrian Aguirre <adrian.aguirre@lifelock.com>
2015-08-26 00:38:28 -0700

    business specific decorator

--- decorator-directives/d-business-specific-decorators/script.js
+++ decorator-directives/d-business-specific-decorators/script.js
@@ -2,31 +2,33 @@
 
 angular.module('app', []);
 
-angular.module('app').controller('MainController', function($scope) {
-  $scope.user1 = {
-    name: 'Luke',
-    //selected: false
-  }
+angular.module('app').controller('MainController', function ($scope) {
+    $scope.user1 = {
+        name: 'Luke',
+        selected: false
+    }
 });
 
-angular.module('app').directive('userTile', function() {
-  return {
-    restrict: 'E',
-    scope: {
-      user: '='
-    },
-    templateUrl: 'userTile.html'
-  }
+angular.module('app').directive('userTile', function () {
+    return {
+        restrict: 'E',
+        scope: {
+            user: '='
+        },
+        templateUrl: 'userTile.html',
+
+    }
 });
 
-//angular.module('app').directive('userClickSelect', function() {
-//  return {
-//    link: function(scope, el, attrs) {
-//      el.on('click', function() {
-//        scope.user.selected = !scope.user.selected;
-//        scope.$apply();
-//      })
-//    }
-//  }
-//})
+angular.module('app').directive('userClickSelect', function () {
+    return {
+        link: function (scope, el, attrs) {
+            el.on('click', function () {
+                scope.user.selected = !scope.user.selected;
+                console.log('clicked');
+                scope.$apply();
+            })
+        }
+    }
+})
 
